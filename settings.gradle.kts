d276edd5daa402ace885c312669b508d0e458679
jansaridhruvi <dhruvijansari21@gnu.ac.in>
2023-10-01 20:44:49 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mad_pr3_036"
+include(":app")
